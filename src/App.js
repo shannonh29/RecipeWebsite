b301b073e68f205f208ec68f0040ff77e8162a62
shannonh29 <72118543+shannonh29@users.noreply.github.com>
2025-05-28 22:10:17 -0500

    Creating Base Website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import star from './star.png';
+import './css/App.css';
+
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1 className="inria-serif-light Title">Recipes</h1>
+        <button className="SignUp" onClick={() => setShowModal(true)}>
+          sign up/register
+        </button>
+      </div>
+
+      <div className="MainContent">
+        <div className="Menu">
+
+          <h3 className="RecipeList">Recipe List</h3>
+          <div className="Line"></div>
+          <div className="Item">
+            <p className="Default">Please Sign In/Register to Begin</p>
+            {/* <img className='star' src={star} alt="star"></img>
+            <div className='Content'>
+              <h4 className="RecipeName"></h4>
+              <h5 className="Description"></h5>
+            </div> */}
+
+          </div>
+        </div>
+
+        <div className="Options">
+          <button className='Option'>Random Pick</button>
+          <button className='Option'>Search</button>
+          <button className='Option'>New Recipe</button>
+          <button className='Option'>Settings</button>
+        </div>
+      </div>
+
+      {showModal && (
+        <div className='modal'>
+          <div className='modal-content'>
+            <span className='close' onClick={() => setShowModal(false)}>&times;</span>
+            <h2>SignUp</h2>
+            <form>
+              <input type="email" placeholder="Email"/>
+              <input type="password" placeholder="Password" />
+              <button type="submit">Sign Up</button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
+
   );
 }
 
