cf60ea0048285aebf26f647e57c793b7843fba2b
shannonh29 <72118543+shannonh29@users.noreply.github.com>
2025-07-24 15:54:48 -0500

    New Recipe Tab Development

--- src/NewRecipe.js
+++ src/NewRecipe.js
@@ -6,18 +6,30 @@ function NewRecipe() {
     const [author, setAuthor] = useState("");
     const [ingredients, setIngredients] = useState("");
     const [directions, setDirections] = useState("");
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Recipe Name: ", name);
+        if(author) {
+            console.log("Author: ", author);
+        }
+        console.log("Ingredients: ", ingredients);
+        console.log("Directions: ", directions);
+    };
+
+
     return (
         <fieldset className="Main_Content">
-            <form action="#" method="get">
-                <label for="name">Recipe Name*</label>
+            <span className='close'>&times;</span>
+            <form onSubmit={handleSubmit}>
+                <label for="name" id="RecipeName">Recipe Name*</label>
                 <input 
                     type="text" 
                     name="name" 
                     id="name" 
                     value={name}
-                    onChange={(e) =>
-                        setName(e.target.value)
-                    }
+                    onChange={(e) => setName(e.target.value)}
                     required 
                 />
                 <label for="author">Author</label>
@@ -26,29 +38,26 @@ function NewRecipe() {
                     name="author"
                     id="author"
                     value={author}
-                    onChange={(e) =>
-                        setAuthor(e.target.value)
-                    }
+                    onChange={(e) => setAuthor(e.target.value)}
                 />
                 <label for="ingredients">Ingredients*</label>
                 <textarea
                     name="ingredients"
                     id="ingredients"
                     value={ingredients}
-                    onChange={(e) =>
-                        setIngredients(e.target.value)
-                    }
+                    onChange={(e) => setIngredients(e.target.value) }
                     required
                 />
                 <label for="directions">Directions*</label>
                 <textarea
                     name="directions"
-                    id="directiions"
+                    id="directions"
                     value={directions}
-                    onChange={(e) =>
-                        setDirections(e.target.value)
-                    }
+                    onChange={(e) => setDirections(e.target.value) }
+                    required
                 />
+
+                <button type="submit" value="submit">Submit</button>
             </form>
         </fieldset>
     );
